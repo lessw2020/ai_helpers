9527f005c85d1323e28c86c7a3f2cd0845121d34
Less Wright <lessw@etrillium.com>
2022-08-21 10:55:17 -0700

    add bfloat16 check for gpu and network

--- helpers/ai_helpers.py
+++ helpers/ai_helpers.py
@@ -1,7 +1,22 @@
 import torch
+from pkg_resources import packaging
+import torch.cuda.nccl as nccl
+import torch.distributed as dist
 
+def check_bfloat_support(check_network_support=True, verbose=True):
+  """ verify hardware support for bfloat16 - gpu and network """
+  gpu_support = torch.version.cuda and torch.cuda.is_bf16_supported()
+  network_support = packaging.version.parse(torch.version.cuda).release >= (11, 0) and dist.is_nccl_available() and nccl.version() >= (2, 10)
 
-def count_model_params(model, verbose = True):     
+  if verbose:
+    print(f"gpu bfloat support = {gpu_support}")
+    if check_network_support:
+      print(f"network bfloat support = {network_support}")
+    
+  return gpu_support if not check_network_support else (gpu_support and network_support)
+
+def count_model_params(model, verbose = True): 
+  """ count total model parameters """
 
   num_params = (sum(p.numel() for p in model.parameters())) / 1e6
   if verbose:
